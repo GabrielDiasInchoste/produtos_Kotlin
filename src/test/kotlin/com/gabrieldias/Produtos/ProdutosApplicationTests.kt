6780abf9a4106e2d5a602e69fe497c4f14d9b094
Gabriel Dias <gabriel.dias97@hotmail.com>
2021-03-22 09:58:22 -0300

    Criado Projeto

---
+++ src/test/kotlin/com/gabrieldias/Produtos/ProdutosApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gabrieldias.Produtos
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProdutosApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
